2f26260a86afa6c5bccfe0f17e90919418687934
junya666 <junshuna.s@gmail.com>
2016-12-24 18:45:44 +0900

    THIS IS FIRST COMMIT

---
+++ todo.sh
@@ -0,0 +1,31 @@
+
+
+usage() {
+
+
+}
+
+add() {
+
+}
+
+list() {
+
+}
+
+done() {
+
+}
+
+remove() {
+
+}
+
+change() {
+
+}
+
+
+SELFDIR=$(cd $(dirname $0); pwd)
+TODO_FILE="$SELFDIR/.todo_list"
+DONE_FILE="$SELFDIR/.done_list"
